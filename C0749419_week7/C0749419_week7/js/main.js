067bc1d647db9b876bfba58d45a864c78b8d4614
jasman92 <msamar1711@gmail.com>
2019-04-01 02:12:05 -0400

    final commiiittt

---
+++ C0749419_week7/C0749419_week7/js/main.js
@@ -0,0 +1,39 @@
+﻿var bird = {
+    genus: "corvus",
+    species: "corvax",
+    commonName: "raven",
+    callType: "squawky",
+    quote: "Nevermore",
+    maxOffspring: 5,
+    noisy: true,
+    deadly: false
+};
+
+bird.color = "black";
+
+var bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonName: "brown bear",
+    callType: "roar",
+    quote: "",
+    maxOffspring: 3,
+    noisy: true,
+    deadly: true
+};
+
+var cities = [
+    'Toronto', 'Ajax', 'Maple', 'Richmond Hill', 'Stouffville', 'Whitby'
+];
+
+var string1 = 'This is the longest string ever.';
+var string2 = 'This is the stronges string ever.';
+var string3 = " Is this the thing called mouunt everest?";
+var string4 = "This is the shermon on the Mount.";
+
+var regex = /this/;
+
+console.log(regex.test(string1));
+console.log(regex.test(string2));
+console.log(regex.test(string3));
+console.log(regex.test(string4));
